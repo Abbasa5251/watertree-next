e56c7dbf2833467ff7c9d0f440a110a495a12238
Abbasa5251 <abbasa5251@hotmail.com>
2023-12-20 17:06:49 +0530

    fix: changed the Container to be div instead of section

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function Container({ children, className }) {
 	return (
-		<section className={cn("mx-auto w-full max-w-7xl", className)}>
+		<div className={cn("mx-auto w-full max-w-7xl", className)}>
 			{children}
-		</section>
+		</div>
 	);
 }
 
