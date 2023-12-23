fa816522cece0822575705eb9034ee0730eb51f9
Abbasa5251 <abbasa5251@hotmail.com>
2023-12-23 18:56:59 +0530

    feat: WIP added contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,77 @@
+import Container from "@/components/Container";
+import { Separator } from "@/components/ui/separator";
+import { siteConfig } from "@/config/site";
+import { Mail, MapPin, Phone } from "lucide-react";
+import React from "react";
+
+const page = () => {
+	return (
+		<>
+			<section className="sm:flex sm:justify-between bg-blue-50">
+				<Container className={"py-28"}>
+					<div className="flex flex-col gap-16 items-center justify-center">
+						<p className="text-center text-balance md:mx-36 mx-12">
+							We appreciate your interest in WaterTree Jones road.
+							Our bottled water, filtration, and pantry services
+							have been in demand for decades. We look forward to
+							hearing how we can provide quality products and
+							excellent services to your home and business!
+						</p>
+						<div className="flex flex-col md:flex-row justify-between items-start mx-8 gap-5">
+							{/* left content */}
+							<div className="flex flex-col">
+								<h1 className="text-xl font-bold text-blue-900 uppercase">
+									Info
+								</h1>
+								<Separator
+									orinetation="horizontal"
+									className="w-1/2 bg-blue-200 mb-4"
+								/>
+								<div className="flex flex-row items-center justify-center gap-2 mb-4">
+									<MapPin className="w-8 h-8 text-blue-900 hidden sm:block" />
+									<p className="text-gray-900 font-medium text-base text-wrap max-w-80">
+										{siteConfig.address}
+									</p>
+								</div>
+								<div className="flex flex-row items-center justify-start gap-2 mb-4">
+									<Phone className="w-8 h-8 text-blue-900 hidden sm:block" />
+									<p className="text-gray-900 font-medium text-base">
+										{siteConfig.contactNumber}
+									</p>
+								</div>
+								<div className="flex flex-row items-center justify-start gap-2 mb-4">
+									<Mail className="w-8 h-8 text-blue-900 hidden sm:block" />
+									<p className="text-gray-900 font-medium text-base">
+										{siteConfig.emailAddress}
+									</p>
+								</div>
+							</div>
+							{/* right content */}
+							<div className="flex flex-col">
+								<h1 className="text-xl font-bold text-blue-900 uppercase">
+									Hydration Hours
+								</h1>
+								<Separator
+									orinetation="horizontal"
+									className="w-1/2 bg-blue-200 mb-4"
+								/>
+								<ul className="list-none">
+									{siteConfig.workingHours.map((wh) => (
+										<li
+											key={wh.day}
+											className="mb-2 text-base font-normal"
+										>
+											{wh.day}: {wh.hours}
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</Container>
+			</section>
+		</>
+	);
+};
+
+export default page;
